dff0683d00239ec4ba8165bd432a6caae5d743ae
Abdumutalibovo <odilbekjuraev07@gmail.com>
2023-01-09 12:36:46 +0500

    add bloc observer

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Fresh Cars',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fresh_cars/app/app.dart';
+import 'package:fresh_cars/app/app_bloc_observer.dart';
 
 void main() {
+  Bloc.observer = AppBlocObserver();
   runApp(const App());
 }
